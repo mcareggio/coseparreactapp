89f56d4013ede94ecdb7746fdee311c568e4e0ce
Marcos Careggio <marcoscareggio@gmail.com>
2025-03-24 22:55:13 -0300

    comienzo menu

--- src/components/HeaderComp.tsx
+++ src/components/HeaderComp.tsx
@@ -1,12 +1,24 @@
-
+import { useId } from "react";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 function HeaderComp() {
+    const openmenubuttonid = useId()
+    const closemenubuttonid = useId()
+
+    function openMenu(event: Event) {
+
+    }
+    function closeMenu(event: Event) { }
     return (
 
         <header className='hd-principal'>
+            <div></div>
             <div className='hd-brand'>COSEPAR</div>
+            <AiOutlineMenu id={openmenubuttonid} className="mainmenuicon openmenuicon" onClick={openMenu} />
+            <AiOutlineClose id={closemenubuttonid} className="mainmenuicon closemenuicon" onClick={closeMenu} />
         </header>
 
     )
 }
 export default HeaderComp
+//
