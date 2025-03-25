77a814ee44f7780a5bab0a897adb1fae7ada0fb5
Marcos Careggio <marcoscareggio@gmail.com>
2025-03-25 06:41:00 -0300

    continuacion menu

--- src/components/HeaderComp.tsx
+++ src/components/HeaderComp.tsx
@@ -1,21 +1,33 @@
-import { useId } from "react";
+import { useEffect, useId } from "react";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 function HeaderComp() {
     const openmenubuttonid = useId()
     const closemenubuttonid = useId()
-
+    const mainmenuid = useId()
+    useEffect(() => {
+        document.getElementsById(mainmenuid).s
+    }, [])
     function openMenu(event: Event) {
 
     }
-    function closeMenu(event: Event) { }
+    function closeMenu(event: Event) {
+
+    }
     return (
 
         <header className='hd-principal'>
             <div></div>
             <div className='hd-brand'>COSEPAR</div>
             <AiOutlineMenu id={openmenubuttonid} className="mainmenuicon openmenuicon" onClick={openMenu} />
-            <AiOutlineClose id={closemenubuttonid} className="mainmenuicon closemenuicon" onClick={closeMenu} />
+            <div className="mainmenu" id={mainmenuid}>
+                <div className="closemenucontainer"><AiOutlineClose id={closemenubuttonid} className="mainmenuicon closemenuicon" onClick={closeMenu} /></div>
+                <ul className="ul-main-menu">
+                    <li><a href="">Inicio</a></li>
+                    <li><a href="">Listado Usuarios Envio Automatico</a></li>
+                    <li><a href="">Listado Facturas Completo</a></li>
+                </ul>
+            </div>
         </header>
 
     )
